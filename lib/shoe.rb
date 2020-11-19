5c10dfaf5888fd172d953fd71168175cb2c84127
Noura <contact.nouraloudani@gmail.com>
2020-11-19 01:34:22 -0500

    Shoe & Book class labs done

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,13 @@
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
+  BRANDS = []
 
   def initialize(brand)
     @brand = brand
+    if (!BRANDS.include?(brand)) 
+      BRANDS << brand
+    end
   end
 
   def cobble
@@ -11,4 +15,11 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
+  # def unique_brands(brand)
+  #   BRANDS.map do |brand| 
+  #     if brand.unique? 
+  #       return BRANDS
+  #     end
+  #   end
+  # end
 end
